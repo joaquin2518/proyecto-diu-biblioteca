2afe032e5ad24044476054c3d92ece045f07969d
Matias <matias.fernandezma@usm.cl>
2024-10-27 20:42:24 -0300

    pagina

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 export const NavBar = () => {
   const navLinkClass = ({ isActive }) => {
@@ -9,40 +10,44 @@ export const NavBar = () => {
   }
   const linkStyle = {
     backgroundColor: '#AD5D4E', // Coffee color
+    color: '#FFFFFF', // White color for text
     padding: '10px',
     borderRadius: '5px',
     margin: '0 5px'
   }
   return (
-    <nav className='nav-bar' style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <NavLink
-        className={navLinkClass}
-        to='/'
-        style={linkStyle}
-      >
-        Inicio
-      </NavLink>
-      <NavLink
-        className={navLinkClass}
-        to='/quienes-somos'
-        style={linkStyle}
-      >
-        Quienes Somos
-      </NavLink>
-      <NavLink
-        className={navLinkClass}
-        to='/apoyo-investigacion'
-        style={linkStyle}
-      >
-        Apoyo Investigación
-      </NavLink>
-      <NavLink
-        className={navLinkClass}
-        to='/apoyo-memoristas'
-        style={linkStyle}
-      >
-        Apoyo Memoristas
-      </NavLink>
+    <nav className='nav-bar' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <img src={logo} alt="Logo" style={{ height: '50px', marginRight: '20px' }} />
+      <div style={{ display: 'flex' }}>
+        <NavLink
+          className={navLinkClass}
+          to='/'
+          style={linkStyle}
+        >
+          Inicio
+        </NavLink>
+        <NavLink
+          className={navLinkClass}
+          to='/quienes-somos'
+          style={linkStyle}
+        >
+          Quienes Somos
+        </NavLink>
+        <NavLink
+          className={navLinkClass}
+          to='/apoyo-investigacion'
+          style={linkStyle}
+        >
+          Apoyo Investigación
+        </NavLink>
+        <NavLink
+          className={navLinkClass}
+          to='/apoyo-memoristas'
+          style={linkStyle}
+        >
+          Apoyo Memoristas
+        </NavLink>
+      </div>
     </nav>
   )
 }
--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-
+import repositoriodigital from '../assets/repositoriodigital.png'
+import bibliotecadigital from '../assets/bibliotecadigital.png'
+import basededatos from '../assets/basededatos.png'
+import guiastematicas from '../assets/guiastematicas.png'
 export const HomePage = () => {
+  const buttonStyle = {
+    fontSize: '31.25px', // 25px * 1.25
+    padding: '6.25px 12.5px', // 5px * 1.25 and 10px * 1.25
+    backgroundColor: '#033655',
+    color: 'white',
+    marginRight: '12.5px', // 10px * 1.25
+    borderRadius: '12.5px' // 10px * 1.25
+  };
+
+  const imageStyle = {
+    width: '100%',
+    maxWidth: '181.25px' // 145px * 1.25
+  };
+
   return (
     <>
-      <p>¡Te damos la bienvenida a la Interfaz energética!</p>
-      <p>En esta página encontraras una 💡 ampolleta que podrás encender y apagar con un botón</p>
+      <button style={buttonStyle}>Prestramo</button>
+      <button style={{ ...buttonStyle, marginRight: '0' }}>Reserva salas</button>
+      <div style={{ display: 'flex', marginTop: '25px' }}> {/* 20px * 1.25 */}
+        <a href="#" style={{ marginRight: '25px' }}>
+          <img src={repositoriodigital} alt="Repositorio Digital" style={imageStyle} />
+        </a>
+        <a href="#">
+          <img src={basededatos} alt="Base de Datos" style={imageStyle} />
+        </a>
+      </div>
+      <div style={{ marginTop: '25px' }}> {/* 20px * 1.25 */}
+        <a href="#" style={{ marginRight: '25px' }}>
+          <img src={bibliotecadigital} alt="Biblioteca Digital" style={imageStyle} />
+          </a>
+          <a href="#">
+          <img src={guiastematicas} alt="Guias Tematicas" style={imageStyle} />
+        </a>
+      </div>
     </>
   )
 }
